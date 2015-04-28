7b46adec3463a1cd07125752548f3096a03d1f95
David Leonard <davidleonard1@Davids-MacBook-Pro.local>
2015-04-28 07:46:14 -0700

    More fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,62 +10,113 @@
 angular.module('todosApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
 
-    function randomColor(format){
 
-      var rint = Math.round(0xffffff * Math.random());
-      switch(format) {
-        case 'hex':
-          return ('#0' + rint.toString(16)).replace(/^#0([0-9a-f]{6})$/i, '#$1');
-        break;
+    // Get the sorted list order from the DOM
+    function getTodos (success) {
 
-        case 'rgb':
-          return 'rgb(' + (rint >> 16) + ',' + (rint >> 8 & 255) + ',' + (rint & 255) + ')';
-        break;
+      // Check for lists
+      var s=0, list=[];
+      $('.todo-list-group').each(function(){
 
-        default:
-          return rint;
-        break;
+          // Init list
+          list.push( initList( $scope.todoLists[s].colors || colorPicker.getColors() ) );
+
+          // Init notes
+          $('#' + this.id + ' .todo-value').each(function(){
+
+            list[s].todos.push( initTodo( this.value, this.style.color ) );
+
+          });
+          s++;
+
+      });
+
+      return list;
+    }
+
+    function initList (pallete) {
+
+      return {
+        'show' : false,
+        'colors' : pallete || colorPicker.getColors(),
+        'todos' : []
+      };
+    };
+
+    function initTodo (item, pallete) {
+
+      return {
+        'item' : item || '',
+        'color' : (pallete.constructor === Array) ? colorPicker.randomColor(pallete) : pallete
       }
     }
 
-    // Get the sorted list order from the DOM
-    function updateTodos(){
+    var colorPicker = {
 
-      // Rebuild the todos list
-      $scope.todos = [];
-      $('.todo-value').each(function(){
-        $scope.$apply();
-        $scope.todos.push({ item: this.value, color: this.style.color });
-        $scope.$apply();
-      });
-    }
+      colors : [
+        ['#79B8E1','#55DEEF','#BAFFFF','#9CB356','#648234','#bfa264','#bbab54','#bb9854','#baffc4'],
+        ['#3FB8AF','#7FC7AF','#DAD8A7','#FF9E9D','#FF3D7F','#C03D7F','#8C627F','#C19E9D','#b1c19d'],
+        ['#AF7575','#EFD8A1','#BCD693','#AFD7DB','#3D9CA8','#749CA8','#AB9CA8','#AB9C86','#9dab9c'],
+        ['#FFBC67','#DA727E','#AC6C82','#7694a5','#5f81af','#735C7B','#649da9','#D89667','#d8c467'],
+        ['#FFFBDC','#BFBCA5','#7F7D6E','#e4e4e4','#E5E2C6','#BAB8A1','#BAB828','#E8E4C8','#e8ef98']
+      ],
+
+      getRandomInt : function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      },
+      randomColor : function (c) {
+        return c[ this.getRandomInt( 0, c.length ) ];
+      },
+      getColors : function () {
+        return this.colors[ this.getRandomInt( 0, this.colors.length ) ];
+      }
+    };
 
     // Get todos saved in localStorageService or create empty array
-    var todosInStore = localStorageService.get('todos');
-    $scope.todos = todosInStore || [];
-
     // Bind localStorageService to $scope.todos
-    $scope.$watch(function(){
+    var todoListsInStore = localStorageService.get('todoLists');
+    $scope.todoLists = todoListsInStore || [];
+    $scope.$watch(function () {
 
-      localStorageService.add('todos', JSON.stringify($scope.todos));
+      localStorageService.add('todoLists', JSON.stringify($scope.todoLists));
     });
 
-    $scope.addTodo = function(){
+    $scope.addList = function () {
 
-      $scope.todos.push({ item: $scope.todo, color: randomColor('rgb') });
-      $scope.todo = '';
+      // Initialize a new list and new blank note (first list lacks a note)
+      // Pass the colors array for random note color from pallete
+      var l = $scope.todoLists.length;
+      $scope.todoLists.push( initList() );
+      if (l>0) $scope.addTodo(l,'a new thing');
+    }
+
+    $scope.addTodo = function (list,item) {
+
+      if ($scope.todoLists[list].todos === undefined) { $scope.todoLists[list].todos = [] }
+      $scope.todoLists[list].todos.push( initTodo( item || 'a new thing', $scope.todoLists[list].colors ) )
+
     };
 
-    $scope.removeTodo = function(i){
+    $scope.removeTodo = function (list,item) {
 
-    	$scope.todos.splice(i,1);
+      // Take out note and empty lists
+      $scope.todoLists[list].todos.splice(item,1);
+      for (var c=0, l=$scope.todoLists.length; c<l; c++) {
+
+        // Remove if list is empty
+        if (!$scope.todoLists[c].todos.length) $scope.todoLists.splice(c,1);
+
+      }
     };
+
+    // Draggable Items
+    $( "#todoApp" ).draggable();
 
     // Sortable Options
     $scope.sortableOptions = {
-        disabled: false,
-        update: function(event) {
-            updateTodos();
+        disabled : false,
+        update : function(event) {
+            $scope.todoLists = getTodos();
             return $scope.sortableUpdated = true;
         }
     };
