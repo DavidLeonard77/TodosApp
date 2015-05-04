fd48b9ef77405a72a3b39fd341d3c0d2a8851b5f
David Leonard <davidleonard1@Davids-MacBook-Pro.local>
2015-05-03 19:07:04 -0700

    Add row class

--- app/scripts/controllers/canvas.js
+++ app/scripts/controllers/canvas.js
@@ -11,6 +11,35 @@ angular.module('todosApp')
 
   .controller('todoCanvasCtrl', function ($scope, localStorageService) {
 
-    // Save the applet canvas locations
+      $scope.colorPicker = {
+
+        colors : [
+          ['#79B8E1','#55DEEF','#BAFFFF','#9CB356','#648234','#bfa264','#bbab54','#bb9854','#baffc4'],
+          ['#3FB8AF','#7FC7AF','#DAD8A7','#FF9E9D','#FF3D7F','#C03D7F','#8C627F','#C19E9D','#b1c19d'],
+          ['#AF7575','#EFD8A1','#BCD693','#AFD7DB','#3D9CA8','#749CA8','#AB9CA8','#AB9C86','#9dab9c'],
+          ['#FFBC67','#DA727E','#AC6C82','#7694a5','#5f81af','#735C7B','#649da9','#D89667','#d8c467'],
+          ['#FFFBDC','#BFBCA5','#7F7D6E','#e4e4e4','#E5E2C6','#BAB8A1','#BAB828','#E8E4C8','#e8ef98']
+        ],
+
+        getRandomInt : function (min, max) {
+          return Math.floor(Math.random() * (max - min)) + min;
+        },
+        randomColor : function (c) {
+          return c[ this.getRandomInt( 0, c.length ) ];
+        },
+        getColors : function () {
+          return this.colors[ this.getRandomInt( 0, this.colors.length ) ];
+        }
+      };
+
+      // Sortable Options
+      $scope.sortableOptions = {
+          disabled : false,
+          update : function(event) {
+              $scope.todoLists = $scope.getTodos();
+              $scope.todoSheets = $scope.getSheets();
+              return $scope.sortableUpdated = true;
+          }
+      };
 
   });
--- app/scripts/controllers/sheets.js
+++ app/scripts/controllers/sheets.js
@@ -15,6 +15,42 @@ angular.module('todosApp')
       templateUrl: '../../applets/sheets.html',
       controller: function ($scope, localStorageService) {
 
+        function initSheet (text, palette, color) {
+
+          var colors = palette || $scope.colorPicker.getColors();
+          return {
+            'text' : text || '',
+            'show' : false,
+            'colors' : colors,
+            'color' : color || $scope.colorPicker.randomColor(colors)
+          };
+        }
+
+        // Get the sorted list order from the DOM
+        $scope.getSheets = function () {
+
+          // Check for lists
+          var list=[];
+          $('todosheets .todo-sheet textarea').each(function(){
+              var d = $('#' + this.id).data();
+              list.push( initSheet( this.value, d.palette, this.style.color ) );
+          });
+
+          return list;
+        };
+
+        $scope.addSheet = function () {
+
+          // Init a new sheet
+          $scope.todoSheets.push( initSheet() );
+        };
+
+        $scope.removeSheet = function (item) {
+
+          // Take out note and empty list if empty
+          $scope.todoSheets.splice(item,1);
+        };
+
         // Get todos saved in localStorageService or create empty array
         // Bind localStorageService to $scope.todos
         var todoSheetsInStore = localStorageService.get('todoSheets');
@@ -23,6 +59,9 @@ angular.module('todosApp')
 
           localStorageService.add('todoSheets', JSON.stringify($scope.todoSheets));
         });
+
+        // Draggable Items
+        $('#todoSheets').draggable({ snap : 'body' });
 
       },
       controllerAs: 'todoSheetsCtrl'
